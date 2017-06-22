d215487041e40895639548ffdfe3802ad8beff0d
aacosta8 <aacosta8@eafit.edu.co>
2017-06-22 16:16:56 -0500

    Sensor de flujo listo

--- testGet/testGet.ino
+++ testGet/testGet.ino
@@ -2,7 +2,7 @@
 // Adaptation done by: Alexander Acosta - Julian Arango - Yeison Osorio
 // github: https://github.com/SISEDomoHouse/ArduinoCodes
 
-// Description: This program test the connection to the server "iotweatherstation" in order to SAVE: idhome, temp, humid and timestamp (yyyy:mm:dd:hh:mm:ss)
+// Description: This program do the connection to the server in ngrok in order to SAVE: id_sensor, id_variable and the value from the sensor
 // the connection is done throught Wifi network and HTTP Connection. It uses GET Method.
 
 //Libraries
@@ -13,17 +13,24 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
+#define sensorDeFlujo D1 // Pin al que esta conectado el sensor
+unsigned int litrosPorMinuto; // Calculated litres/hour
+unsigned long pulsosAcumulados; // Pulsos acumulados
+volatile int pulsos; // Cantidad de pulsos del sensor. Como se usa dentro de una interrupcion debe ser volatile
+float litros[2]; // Litros acumulados
+
 const unsigned long SEND_Timer = 5000;  //Delay between SEND updates, 5000 milliseconds
-const unsigned int SENTVARS = 1;  //Number of sensor vars sent to REST Web App, (idHome, Temp & Humid, TimeStamp)
+const unsigned int SENTVARS = 3;  //Number of variables (sensor_id, variable_id & value)
 const char* SSID = "domohouse"; //WiFi SSID, change nombre-wifi por la red propia
 const char* PASSWORD = "1234567890"; //WiFi Pass, coloque el password real
-const char* HOST = "http://server";  //REST Web Host, replace by real 'server' url
+const char* HOST = "http://959a5423.ngrok.io";  //REST Web Host, replace by real 'server' url
 
-String appName = "/sensor";
-String serviceSaveSensors = "/create";  //Name of the service SAVESENSORS
-char* propertyNames[] = {"name"}; //Vector Var names
+String appName = "/register";
+String serviceSaveRegister = "/create";  //Name of the service to create registers
+char* propertyNames[] = {"sensor", "variable", "value"}; //Vector Var names
 String propertyValues[SENTVARS]; //Vector for Var values
 unsigned long lastConnectionTime = 0; //Last time you connected to the server, in milliseconds
+
 
 
 void SEND(int SENTVARS, char* sensorNames[], String values[]) {
@@ -31,7 +38,7 @@ void SEND(int SENTVARS, char* sensorNames[], String values[]) {
   if ((WiFiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
     String url = appName;
-    url += serviceSaveSensors;
+    url += serviceSaveRegister;
     url += "?";
     for (int idx = 0; idx < SENTVARS; idx++)
     {
@@ -59,8 +66,13 @@ void SEND(int SENTVARS, char* sensorNames[], String values[]) {
     } else {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
+    litros[1] = values[2].toFloat();
     http.end();
   }
+}
+
+void flujo () {
+  pulsos++;
 }
 
 
@@ -68,6 +80,10 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+  propertyValues[0] = "1"; // sensor_id
+  propertyValues[1] = "1"; // variable_id
+  litros[1]= -1;
+  pinMode(sensorDeFlujo, INPUT);
 
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] WAIT %d...\n", t);
@@ -75,16 +91,34 @@ void setup() {
     delay(1000);
   }
 
+  attachInterrupt(digitalPinToInterrupt(sensorDeFlujo), flujo, RISING);
+  interrupts(); // Habilitar interrupciones
+
   WiFiMulti.addAP(SSID, PASSWORD);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (millis() - lastConnectionTime > SEND_Timer) {
-    propertyValues[0] = "sensor";
-    SEND(SENTVARS, propertyNames, propertyValues);
-    lastConnectionTime = millis();
-  }
+    
+    litros[0] = pulsos*1.0/400;
+    
+    if (millis() - lastConnectionTime > SEND_Timer) {
+      propertyValues[0] = "1";
+      propertyValues[1] = "1";
+      propertyValues[2] = litros[0];
+      Serial.print(litros[0]);
+      Serial.print(" ");
+      Serial.println(litros[1]);
+      if(litros[0]>litros[1]){
+        SEND(SENTVARS, propertyNames, propertyValues);
+        Serial.println("Entró");
+      }
+      lastConnectionTime = millis();
+    }
+    
+    
+
+
 
 }
